a49cf97402af8007eaf4e665f7c5c86edae7bdf1
Blessingdev233 <blessing.lartey@gmail.com>
2022-03-11 23:08:22 +0000

    C code that prints all possible combinations of 2-digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: printing combination of 2-digits
+ *
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	int number;
+	int number1;
+
+	number = 48;
+	while (number <= 57)
+	{
+		number1 = 0;
+		while (number1 <= 57)
+		{
+			if (number < number1)
+			{
+				putchar(number);
+				putchar(number1);
+				if (number < 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			number1++;
+		}
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
