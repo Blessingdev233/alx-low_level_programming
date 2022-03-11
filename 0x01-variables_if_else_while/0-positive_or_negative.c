3453078600733fa6ae2c0c7116d87f213bfa64ca
Blessingdev233 <blessing.lartey@gmail.com>
2022-03-11 17:52:04 +0000

    fixing betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,20 @@
 #include <stdlib.h>
-#include <time.h> 
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Entry point of code
- * 
- * Description: Gets the last number and checks if it is positive zero or negative
- * 
- * Return: Always 0 
+ * main - Entry point
+ *
+ * Description: prints a random number and evaluates if it's positive, negative
+ * or zero
+ *
+ * Return: Always 0
  */
 
 int main(void)
 {
-        int n;
-        
+	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
@@ -22,5 +23,5 @@ int main(void)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is positive\n", n);
-        return 0;
+	return (0);
 }
