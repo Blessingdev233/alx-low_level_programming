3797528c59259e5374f5cb6fcfc195c913c95e75
Blessingdev233 <blessing.lartey@gmail.com>
2022-03-28 20:29:56 +0000

    Moves a pointer to a string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - Moves a pointer to a string
+ * @s: Pointer to pointer string
+ * @to: Pointer char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
