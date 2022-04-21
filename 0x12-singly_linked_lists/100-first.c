879bb47f23c88c684fc42d15c9636d31651123b9
Blessingdev233 <blessing.lartey@gmail.com>
2022-04-21 20:53:38 +0000

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void start_up_func(void)__attribute__((constructor));
+/**
+ * start_up_func - runs before main.
+ *
+ * Return: void function
+ */
+void start_up_func(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
