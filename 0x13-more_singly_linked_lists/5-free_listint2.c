b884a4905e26a9a997eb69e48f184943844aa441
Blessingdev233 <blessing.lartey@gmail.com>
2022-04-26 08:43:12 +0000

    frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees linked lists.
+ * @head: pointer to a struct
+ *
+ * Return: void function
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head != NULL)
+	{
+		while (*head != NULL)
+		{
+			temp = *head;
+			(*head) = (*head)->next;
+			free(temp);
+		}
+		(*head) = NULL;
+	}
+}
