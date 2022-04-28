465c6e7c9a706ca4dbbe7477e9f2bab70347d242
Blessingdev233 <blessing.lartey@gmail.com>
2022-04-28 16:34:39 +0000

    first

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - turns binary to uint
+ * @b: character
+ *
+ * Return: unsigned int of char
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+	unsigned int base;
+	int i;
+	const char *a = b;
+
+	base = 1;
+	if (b == NULL)
+		return (0);
+	while (*a)
+	{
+		if ((*a != '0' && *a != '1'))
+			return (0);
+		a++;
+	}
+	for (i = _strlen(b) - 1; i >= 0; i--)
+	{
+		if (b[i] == '1')
+		{
+			n += base;
+		}
+		base *= 2;
+	}
+	return (n);
+}
+
+/**
+ * _strlen - return the length of a string
+ * @s: string to be checked
+ *
+ * Return: lenght of string
+ */
+
+unsigned int _strlen(const char *s)
+{
+	int var;
+	int result = 0;
+
+	for (var = 0; s[var] != '\0'; var++)
+	{
+		result += 1;
+	}
+	return (result);
+}
+
