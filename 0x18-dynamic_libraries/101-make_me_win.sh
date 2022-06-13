6e38c9354ff84aee7aded9a21e277f243f640d39
Blessingdev233 <blessing.lartey@gmail.com>
2022-06-13 23:00:00 +0000

    win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --output-document=$HOME/idontknow.so https://github.com/Blessingdev233/alx-low_level_programming/blob/main/0x18-dynamic_libraries/nrandom.so?raw=true
+wget -q --output-document=$HOME/nrandom.so https://raw.github.com/Blessingdev233/alx-low_level_programming/blob/main/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=$HOME/nrandom.so
